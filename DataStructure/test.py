7f1ad9b3cc84e89d5c3d94ce6394569af5d0968e
lyz21 <liuyenzhe@163.com>
2020-07-01 23:06:47 +0800

    添加AS

--- DataStructure/test.py
+++
@@ -1,8 +0,0 @@
-# encoding=utf-8
-"""
-@Time : 2020/5/29 15:25 
-@Author : LiuYanZhe
-@File : test.py 
-@Software: PyCharm
-@Description: 
-"""
--- math_model/test/test.py
+++
@@ -1,8 +0,0 @@
-# encoding=utf-8
-"""
-@Time : 2020/6/21 20:07 
-@Author : LiuYanZhe
-@File : test.py 
-@Software: PyCharm
-@Description: 一些基本不会复用的方法
-"""
