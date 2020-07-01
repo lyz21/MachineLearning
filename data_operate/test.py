4310686b824aad277dc723c1233993b44da048cb
lyz21 <liuyenzhe@163.com>
2020-07-01 23:17:50 +0800

    添加data_operate

---
+++ data_operate/test.py
@@ -0,0 +1,59 @@
+# encoding=utf-8
+"""
+@Time : 2020/2/4 14:49 
+@Author : LiuYanZhe
+@File : test.py 
+@Software: PyCharm
+@Description: 小测试
+"""
+import numpy as np
+
+# a=np.ceil(10/3)
+# a=np.random.randint(10)
+# print(a)
+
+# data=np.loadtxt('data.txt')
+# print(len(data))
+# print(len(data))
+
+# range1=range(10)
+# print(range1)
+# range1=list(range1)
+# print(range1)
+
+# a0=[]
+# a = [1, 2, 3]
+# a0.append(a)
+# print(a0)
+
+# a=[]
+# print(a!=[])
+
+# a = [['.', '.', '.', '0', '.', '.', '.', ],
+#      ['.', '.', '0', '0', '0', '.', '.', ],
+#      ['.', '0', '0', '0', '0', '0', '.', ],
+#      ['.', '0', '0', '0', '0', '0', '.', ],
+#      ['.', '.', '0', '0', '0', '.', '.', ],
+#      ['.', '.', '.', '0', '.', '.', '.', ],
+#      ]
+# # 原版输出
+# print('原版输出')
+# for i in range(len(a)):
+#     for j in range(len(a[0])):
+#         print(a[i][j],end='')
+#     print()
+# # 转置输出
+# print('转置输出')
+# for i in range(len(a[0])):
+#     for j in range(len(a)):
+#         print(a[j][i],end='')
+#     print()
+
+# a = [1, 2, 3]
+# b = a.copy()
+# b.remove(1)
+# print(a, ';', b)
+
+for i in range(10):
+    if i<3:
+        print(i)
